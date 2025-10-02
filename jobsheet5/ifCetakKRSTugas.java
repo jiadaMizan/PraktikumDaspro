b0732bf7508d4c31f09062f6637998cae6852133
jiadaMizan <jiada1308@gmail.com>
2025-10-02 10:07:34 +0700

    Tugas1

---
+++ jobsheet5/ifCetakKRSTugas.java
@@ -0,0 +1,26 @@
+package jobsheet5;
+
+import java.util.Scanner;
+public class ifCetakKRSTugas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        }
+
+        String hasil = uktLunas 
+            ? "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA"
+            : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+        
+        System.out.println("\n[Versi Ternary Operator]");  
+        System.out.println(hasil);
+    }
+}
--- jobsheet5/ifElseCetakKRS16.java
+++ jobsheet5/ifElseCetakKRS16.java
@@ -9,10 +9,24 @@ public class ifElseCetakKRS16 {
         System.out.print("Masukkan semester saat ini: ");
         int semester = sc.nextInt();
 
-        if (semester >= 1 && semester <= 8) {
-            System.out.println("KRS Semester " + semester + " ditampilkan");
+        if (semester == 1) {
+            System.out.print("KRS Semester 1 ditampilkan");
+        } else if (semester == 2) {
+            System.out.print("KRS Semester 2 ditampilkan");
+        } else if (semester == 3) {
+            System.out.print("KRS Semester 3 ditampilkan");
+        } else if (semester == 4) {
+            System.out.print("KRS Semester 4 ditampilkan");
+        } else if (semester == 5) {
+            System.out.print("KRS Semester 5 ditampilkan");
+        } else if (semester == 6) {
+            System.out.print("KRS Semester 6 ditampilkan");
+        } else if (semester == 7) {
+            System.out.print("KRS Semester 7 ditampilkan");
+        } else if (semester == 8) {
+            System.out.print("KRS Semester 8 ditampilkan");
         } else {
-            System.out.println("Semester tidak valid");
+            System.out.print("Semester tidak valid");
         }
     }
 }
