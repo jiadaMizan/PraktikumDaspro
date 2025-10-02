360c807985b1a397ddf6842acefcd410f596018f
jiadaMizan <jiada1308@gmail.com>
2025-10-02 10:30:44 +0700

    Tugas2

---
+++ jobsheet5/tugas2.java
@@ -0,0 +1,18 @@
+package jobsheet5;
+
+import java.util.Scanner;
+public class tugas2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Masukkan angka: ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Angka tersebut merupakan angka genap");
+        } else {
+            System.out.println("Angka tersebut merupakan angka ganjil");
+        }
+
+    }
+}
